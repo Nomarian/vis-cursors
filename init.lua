e6072dec2ef0f88792fc67fa4b7db3bfb2865d08
Nomarian <nomarian@outlook.com>
2025-01-05 01:09:28 -0400

    current

--- init.lua
+++ init.lua
@@ -1,46 +1,70 @@
+require"vis"
+local vis = vis
+
 local M = {}
 local cursors = {}
 local files = {}
 
--- default maxsize
+--[[ TODO:
+	files is a dictionary
+	open as w+, seek to the number if it exists, save from then on
+--]]
+
+-- default cachefile maxsize
 M.maxsize = 1000
 
+local Exists = function -- string|false
+(
+	filepath -- string
+)
+	return os.rename(filepath, filepath) and filepath or false
+end
+
 -- get the default system cache directory
+-- (Exists($XDG_CACHE_HOME or $HOME/.cache)/vis-cursors) or $HOME/.vis-cursors
 local get_default_cache_path = function()
+	local filename = "vis-cursors"
 	local HOME = os.getenv('HOME')
-	local XDG_CACHE_HOME = os.getenv('XDG_CACHE_HOME')
-	local BASE = XDG_CACHE_HOME or HOME
-	return BASE .. '/.vis-cursors'
+	local home = HOME .. "/." .. filename
+
+	-- freedesktop states that if $XDG_CACHE_HOME is not set
+	-- $HOME/.cache must be used (if it exists)
+	local XDG_CACHE_HOME = Exists(
+		os.getenv"XDG_CACHE_HOME"
+		or (HOME .. "/.cache")
+	)
+	if XDG_CACHE_HOME then
+		XDG_CACHE_HOME = XDG_CACHE_HOME .. "/" .. filename
+		-- move vis-cursors to new location & behavior
+		-- this fails, but it doesn't error so it doesn't matter
+		os.rename(home, XDG_CACHE_HOME)
+		return XDG_CACHE_HOME
+	end
+
+	return home
 end
 
 -- default save path
 M.path = get_default_cache_path()
 
-function read_files()
-
-	-- read file
+local read_files = function ()
 	local file = io.open(M.path)
 	if file == nil then
 		return
 	end
 
-	files = {}
-
 	-- read positions per file path
 	for line in file:lines() do
-		for path, pos in string.gmatch(line, '(.+)[,%s](%d+)') do
-			cursors[path] = pos
-			table.insert(files, path)
-		end
+		local path, pos = string.match(line, '^(.+)[,%s](%d+)$')
+		cursors[path] = pos
+		table.insert(files, path)
 	end
 
 	file:close()
 end
 
 -- read cursors from file on init
-local on_init = function()
-	read_files()
-end
+local on_init = read_files
 
 -- apply cursor pos on win open
 local on_win_open = function(win)
@@ -74,6 +98,7 @@ local on_win_close = function(win)
 	read_files()
 
 	-- remove old occurences of current path
+	-- TODO: use dictionary?
 	for i, path in ipairs(files) do
 		if path == win.file.path then
 			table.remove(files, i)
@@ -84,7 +109,6 @@ local on_win_close = function(win)
 	if win.selection.pos == 0 then
 		return
 	end
-
 
 	-- insert current path to top of files
 	table.insert(files, 1, win.file.path)
@@ -103,11 +127,10 @@ local on_quit = function()
 
 	-- buffer cursors string
 	local buffer = {}
-	for i, path in ipairs(files) do
-		table.insert(buffer, string.format('%s,%d', path, cursors[path]))
-		if M.maxsize and #buffer >= M.maxsize then
-			break
-		end
+	local limit = #files<=M.maxsize and #files or M.maxsize
+	for i=1, limit do
+		local path = files[i]
+		buffer[i] = string.format('%s,%d', path, cursors[path])
 	end
 	local output = table.concat(buffer, '\n')
 	file:write(output)
